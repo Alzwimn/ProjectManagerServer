14e017470f16517c317872bd3cb5343951dfa6c4
Alzwimn <alzwimn@gmail.com>
2020-11-24 13:37:57 -0300

    stop tracking compiled files

--- dist/src/Server/Utils.js
+++
@@ -1,9 +0,0 @@
-import { parse } from "url";
-export class Utils {
-    static getUrlBasePath(url) {
-        if (!url)
-            return "";
-        const parsedUrl = parse(url);
-        return parsedUrl.pathname.split("/")[1];
-    }
-}
